3c64ad87d9c9258a6c17501a71aa2e34c2a69900
Dinesh Kapri <49795881+kapridinesh99@users.noreply.github.com>
2023-09-16 06:57:41 +0530

    Updated Error

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -10,28 +10,6 @@ function Footer() {
         <div className="wave" id="wave3"></div>
         <div className="wave" id="wave4"></div>
       </div>
-      <ul className="social-icon">
-        <li className="social-icon__item">
-          <a className="social-icon__link" href="#">
-            <ion-icon name="logo-facebook"></ion-icon>
-          </a>
-        </li>
-        <li className="social-icon__item">
-          <a className="social-icon__link" href="#">
-            <ion-icon name="logo-twitter"></ion-icon>
-          </a>
-        </li>
-        <li className="social-icon__item">
-          <a className="social-icon__link" href="#">
-            <ion-icon name="logo-linkedin"></ion-icon>
-          </a>
-        </li>
-        <li className="social-icon__item">
-          <a className="social-icon__link" href="#">
-            <ion-icon name="logo-instagram"></ion-icon>
-          </a>
-        </li>
-      </ul>
       <ul className="menu">
         <li className="menu__item">
           <a className="menu__link" href="#">
