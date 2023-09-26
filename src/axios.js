224aeb959a54ffa69a24190872ff1a5dddd476b7
Tejas Asthana <tejasA82@gmail.com>
2023-09-27 00:51:33 +0530

    hardcode apis

--- src/axios.js
+++ src/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const token = localStorage.getItem('rv_token') || '';
 
 export const resourceService = axios.create({
-    baseURL: process.env.REACT_APP_RESOURCE_URL,
+    baseURL: 'https://u6vywhlh59.execute-api.ap-south-1.amazonaws.com/',
     headers: {
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${token}`,
@@ -11,7 +11,7 @@ export const resourceService = axios.create({
 });
 
 export const authService = axios.create({
-    baseURL: process.env.REACT_APP_RESOURCE_URL,
+    baseURL: 'https://u6vywhlh59.execute-api.ap-south-1.amazonaws.com/',
     headers: {
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${token}`,
