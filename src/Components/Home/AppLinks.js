b32560e0a930fa02c0e2fa4959581323002a9ac7
Tejas Asthana <tejasa82@gmail.com>
2023-11-04 13:48:26 +0530

    fixes

--- src/Components/Home/AppLinks.js
+++ src/Components/Home/AppLinks.js
@@ -8,11 +8,12 @@ function AppLinks() {
         <h3>Download Real Value APP Today and Earn More</h3>
         <div className="flex gap-l svg-links">
           <img src="/AppStore.svg" className="cursor-pointer" alt="appStore" />
-          <img
-            src="/googlePlay.svg"
-            className="cursor-pointer"
-            alt="googlePlay"
-          />
+          <a href='https://play.google.com/store/apps/details?id=com.realvalues.realvalues' alt='google play link' >
+            <img
+              alt="googlePlay" 
+              src="/googlePlay.svg"
+              className="cursor-pointer" />
+          </a>
         </div>
       </div>
       <div>
